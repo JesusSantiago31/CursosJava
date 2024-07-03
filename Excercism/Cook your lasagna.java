9a969193c0e7bfec68404cd8aad103f6b063930f
JesusSantiago31 <160097941+JesusSantiago31@users.noreply.github.com>
2024-07-02 22:40:29 -0600

    Create  Cook your lasagna.java

---
+++ Excercism/Cook your lasagna.java
@@ -0,0 +1,19 @@
+ //First program i made on the plataform
+public class Lasagna {    
+    
+    public int expectedMinutesInOven(){
+        return 40;
+    }
+    
+    public int remainingMinutesInOven(int x){
+        return expectedMinutesInOven() - x;
+    }
+    
+    public int preparationTimeInMinutes(int can){
+        return can * 2;
+    }
+        
+    public int totalTimeInMinutes(int la, int ov){
+        return preparationTimeInMinutes(la) + ov;
+    }
+}
